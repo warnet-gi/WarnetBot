b9851b01be9861a2e5d657f908a525560079997f
Iqrar Agalosi Nureyza <=>
2022-10-31 11:28:12 +0700

    Implement simple bot setup

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,8 +1,30 @@
-from unicodedata import name
+import os
+import discord
+from discord import Intents
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+
+client = commands.Bot(
+    command_prefix='war!',
+    intents=Intents.all()
+)
+
+@client.event
+async def on_ready():
+    print("The bot is online!")
+    print("------------------")
+
+
+@client.command()
+async def hello(ctx):
+    await ctx.send("Hello")
 
 
 def main():
-    print("Hello World")
+    client.run(token=os.getenv('BOT_TOKEN'))
+
 
 if __name__ == "__main__":
     main()
