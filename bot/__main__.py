1046033d438a9cd731b857b840ebdb6d9bd9cc46
Iqrar Agalosi Nureyza <=>
2022-10-31 14:32:33 +0700

    Change the main structure of the bot codes

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,35 +1,58 @@
 import os
+import aiohttp
+import asyncio
+
 import discord
-from discord import Intents
-from dotenv import load_dotenv
 from discord.ext import commands
+
+from dotenv import load_dotenv
 
 load_dotenv()
 
-client = commands.Bot(
-    command_prefix='war!',
-    strip_after_prefix=True,
-    intents=Intents.all(),
-)
+BOT_PREFIX = 'war!'
 
-@client.event
-async def on_ready():
-    await client.change_presence(
-        status=discord.Status.idle,
-        activity=discord.Activity(type=discord.ActivityType.watching, name='Pengguna WARNET')
-    )
-    
-    print("The bot is online!")
-    print("------------------")
+class WarnetBot(commands.Bot):
+    debug: bool    
+    bot_app_info: discord.AppInfo
 
+    def __init__(self) -> None:
+        super().__init__(command_prefix=BOT_PREFIX, strip_after_prefix=True, intents=discord.Intents.all())
+        self.session: aiohttp.ClientSession = None
 
-@client.command()
-async def hello(ctx):
-    await ctx.send("Hello")
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("The bot is online!")
+        print("Logged in as {}".format(self.user))
+        print("------------------")
+
+        await self.change_presence(
+            status=discord.Status.idle,
+            activity=discord.Activity(type=discord.ActivityType.watching, name='Pengguna WARNET')
+        )
+
+    async def setup_hook(self):
+        return await super().setup_hook()
+
+    async def load_cogs(self):
+        initial_extension = []
+        for filename in os.listdir('./cogs'):
+            if filename.endswith('.py'):
+                initial_extension.append('cogs.' + filename[:-3])
+        
+        await self.load_extension(initial_extension)
+
+    async def close(self):
+        await self.session.close()
+        await super().close()
+
+    async def start(self, debug: bool = False):
+        self.debug = debug
+        return await super().start(os.getenv('BOT_TOKEN'), reconnect=True)
 
 
 def main():
-    client.run(token=os.getenv('BOT_TOKEN'))
+    bot = WarnetBot()
+    asyncio.run(bot.start())
 
 
 if __name__ == "__main__":
