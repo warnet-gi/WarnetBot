cf94e2d69fce47a23860519eb41a4e625f98b6c1
Iqrar Agalosi Nureyza <misteriqrar@gmail.com>
2022-11-10 20:05:05 +0700

    Hide error message when closing bot by doing keyboard interrupt

--- bot/__main__.py
+++ bot/__main__.py
@@ -3,8 +3,10 @@ from bot.bot import WarnetBot
 
 def main():
     bot = WarnetBot()
-    asyncio.run(bot.start())
-
+    try:
+        asyncio.run(bot.start())
+    except KeyboardInterrupt:
+        print("Logging Out...")
 
 if __name__ == "__main__":
     main()
