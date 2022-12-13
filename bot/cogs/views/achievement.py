0c1c17c9a9d2509032ab412784cd7516efd9827f
Iqrar Agalosi Nureyza <misteriqrar@gmail.com>
2022-12-13 20:08:16 +0700

    Refactor Confirm view

--- bot/cogs/views/achievement.py
+++ bot/cogs/views/achievement.py
@@ -7,22 +7,6 @@ from discord.ext import commands
 from typing import Optional, Dict
 
 
-class Confirm(discord.ui.View):
-    def __init__(self):
-        super().__init__()
-        self.value = None
-
-    @discord.ui.button(label='Ya', style=discord.ButtonStyle.danger)
-    async def confirm(self, interaction: Interaction, button: discord.ui.Button):
-        self.value = True
-        self.stop()
-    
-    @discord.ui.button(label='Tidak', style=discord.ButtonStyle.primary)
-    async def cancel(self, interaction: Interaction, button: discord.ui.Button):
-        self.value = False
-        self.stop()
-
-
 class StatsProgressDetail(discord.ui.View):
     def __init__(self, *,
                  timeout: int = 60,
