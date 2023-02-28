b3749784644a1a9b41e9fff914adc0c609c02107
Monnie <misteriqrar@gmail.com>
2023-02-28 10:53:04 +0700

    Implement scheduled message feature (#58)
    * Add database to store message scheduling task
    
    * Add a hybrid command to schedule message
    
    * Format Python code with psf/black (#59)
    
    Co-authored-by: Iqrar99 <Iqrar99@users.noreply.github.com>
    
    * Add keyword-only arguments
    
    * Add guild id into database
    
    * Add an alias for hybrid group
    
    * Add edit and list command into message scheduling feature
    
    * Add permission gate on edit and list command
    
    * Implement a command to cancel scheduled message
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Iqrar99 <Iqrar99@users.noreply.github.com>

--- bot/cogs/admin.py
+++ bot/cogs/admin.py
@@ -1,20 +1,25 @@
 import discord
-from discord import Interaction, app_commands, ui
-from discord.ext import commands
+from discord import Interaction, app_commands
+from discord.ext import commands, tasks
 
 from bot.config import config
 from bot.bot import WarnetBot
 from bot.cogs.ext.tcg.utils import send_missing_permission_error_embed
 
-import io
-import datetime, time
-from typing import Optional, Literal
+import re
+from datetime import datetime, timedelta
+from typing import Optional, Literal, Union, Tuple
 
 
 @commands.guild_only()
 class Admin(commands.GroupCog, group_name="admin"):
     def __init__(self, bot: WarnetBot) -> None:
         self.bot = bot
+        self.db_pool = self.bot.get_db_pool()
+
+    @commands.Cog.listener()
+    async def on_connect(self) -> None:
+        self._message_schedule_task.start()
 
     @commands.command()
     @commands.is_owner()
@@ -104,7 +109,7 @@ class Admin(commands.GroupCog, group_name="admin"):
                 color=discord.Color.green(),
                 title='✅ Role successfully given',
                 description=f"Role {role.mention} telah diberikan kepada **{cnt}** member di voice channel {vc.mention}.",
-                timestamp=datetime.datetime.now(),
+                timestamp=datetime.now(),
             )
             embed.set_footer(
                 text=f'Given by {str(interaction.user)}',
@@ -169,6 +174,239 @@ class Admin(commands.GroupCog, group_name="admin"):
             await interaction.response.send_message(
                 content="You don't have permission to execute this command!", ephemeral=True
             )
+
+    @commands.hybrid_group(name='schedule-message', aliases=['smsg'])
+    async def schedule_message(self, ctx: commands.Context) -> None:
+        await ctx.send_help(ctx.command)
+
+    @schedule_message.command(name='add', description='Add a message to be scheduled on a channel.')
+    @app_commands.describe(
+        channel='Channel target where the message will be sent later.',
+        time='Relative time e.g. 1d, 2h, 40m, 20s, and can be combined like 5h10m20s.',
+        message='Message to be scheduled.',
+    )
+    async def schedule_message_add(
+        self,
+        ctx: commands.Context,
+        channel: Union[discord.TextChannel, discord.Thread, discord.ForumChannel],
+        time: str,
+        *,
+        message: str,
+    ) -> None:
+        await ctx.typing()
+        if not ctx.author.guild_permissions.manage_channels:
+            return await ctx.send(
+                content="❌ You don't have permission to execute this command!", ephemeral=True
+            )
+
+        if len(message) > 2000:
+            return await ctx.send(
+                content="❌ Message failed to sent. Message can't exceed 2000 characters.",
+                ephemeral=True,
+            )
+        message = '\n'.join(message.split('\\n'))  # support newline in slash command
+
+        parsed_time = self._parse_relative_time(time)
+        if parsed_time is not None:
+            day, hour, minute, second = parsed_time
+        else:
+            return await ctx.send(
+                content="❌ Wrong relative time format.",
+                ephemeral=True,
+            )
+
+        date_now = datetime.now()
+        date_trigger = date_now + timedelta(days=day, hours=hour, minutes=minute, seconds=second)
+
+        if date_trigger <= date_now:
+            return await ctx.send('❌ You must specify a time in the future.', ephemeral=True)
+
+        async with self.db_pool.acquire() as conn:
+            await conn.execute(
+                'INSERT INTO scheduled_message (guild_id, channel_id, message, date_trigger) VALUES ($1, $2, $3, $4);',
+                ctx.guild.id,
+                channel.id,
+                message,
+                date_trigger,
+            )
+
+        if self._message_schedule_task.is_running():
+            self._message_schedule_task.restart()
+        else:
+            self._message_schedule_task.start()
+
+        await ctx.send(
+            f'⏰ Your message will be triggered in {channel.mention} <t:{int(date_trigger.timestamp())}:R>'
+        )
+
+    @schedule_message.command(name='edit', description='Edit a scheduled message.')
+    @app_commands.describe(
+        scheduled_message_id='Message schedule id',
+        new_message='New edited message.',
+    )
+    async def schedule_message_edit(
+        self,
+        ctx: commands.Context,
+        scheduled_message_id: commands.Range[int, 1],
+        *,
+        new_message: str,
+    ) -> None:
+        if not ctx.author.guild_permissions.manage_channels:
+            return await ctx.send(
+                content="❌ You don't have permission to execute this command!", ephemeral=True
+            )
+
+        async with self.db_pool.acquire() as conn:
+            res = await conn.fetchval(
+                "SELECT id FROM scheduled_message WHERE id=$1;", scheduled_message_id
+            )
+
+            if res is not None:
+                await conn.execute(
+                    'UPDATE scheduled_message SET message=$1 WHERE id=$2;',
+                    new_message,
+                    scheduled_message_id,
+                )
+
+            else:
+                not_found_message = (
+                    f'There is no scheduled message with id `{scheduled_message_id}`.\n\n'
+                )
+                not_found_message += 'Use `/admin schedule-message list` or `war! smsg list` to check the list of scheduled messages.'
+                return await ctx.send(not_found_message)
+
+        await ctx.send(
+            f'Message is succesfully edited on scheduled message id `{scheduled_message_id}`'
+        )
+
+    @schedule_message.command(name='cancel', description='Cancel a scheduled message.')
+    @app_commands.describe(scheduled_message_id='Message schedule id to be canceled.')
+    async def schedule_message_cancel(
+        self, ctx: commands.Context, scheduled_message_id: commands.Range[int, 1]
+    ) -> None:
+        if not ctx.author.guild_permissions.manage_channels:
+            return await ctx.send(
+                content="❌ You don't have permission to execute this command!", ephemeral=True
+            )
+
+        async with self.db_pool.acquire() as conn:
+            res = await conn.fetchval(
+                "SELECT id FROM scheduled_message WHERE id=$1;", scheduled_message_id
+            )
+
+            if res is not None:
+                await conn.execute(
+                    'DELETE FROM scheduled_message WHERE id=$1', scheduled_message_id
+                )
+                return await ctx.send(
+                    f'Scheduled message id `{scheduled_message_id}` has been canceled.'
+                )
+
+            else:
+                not_found_message = (
+                    f'There is no scheduled message with id `{scheduled_message_id}`.\n\n'
+                )
+                not_found_message += 'Use `/admin schedule-message list` or `war! smsg list` to check the list of scheduled messages.'
+                return await ctx.send(not_found_message)
+
+    @schedule_message.command(
+        name='list', description='Show the list of active scheduled messages in the guild.'
+    )
+    async def schedule_message_list(self, ctx: commands.Context) -> None:
+        if not ctx.author.guild_permissions.manage_channels:
+            return await ctx.send(
+                content="❌ You don't have permission to execute this command!", ephemeral=True
+            )
+
+        async with self.db_pool.acquire() as conn:
+            records = await conn.fetch(
+                'SELECT * FROM scheduled_message WHERE guild_id=$1', ctx.guild.id
+            )
+
+        scheduled_message_data_list = [dict(record) for record in records]
+
+        content = f'List of Scheduled message in **{ctx.guild.name}**\n'
+        is_replied = False
+        for data in scheduled_message_data_list:
+            channel = ctx.guild.get_channel(data['channel_id'])
+            date_trigger_unix = int(data['date_trigger'].timestamp())
+            message = data['message']
+            content_message = message if len(message) <= 20 else message[:20] + '...'
+            content_extra = f"**{data['id']}**: {channel.mention}: {content_message} - <t:{date_trigger_unix}:F> <t:{date_trigger_unix}:R>\n"
+
+            if len(content + content_extra) > 2000:
+                if not is_replied:
+                    is_replied = True
+                    await ctx.reply(content=content, mention_author=False)
+                else:
+                    await ctx.channel.send(content=content)
+
+                content = ''
+
+            content += content_extra
+
+        content += "\nCancel a message schedule by using `/admin schedule-message cancel <id>` or `war! smsg cancel <id>`"
+        if is_replied:
+            await ctx.channel.send(content=content)
+        else:
+            await ctx.reply(content=content, mention_author=False)
+
+    @tasks.loop()
+    async def _message_schedule_task(self) -> None:
+        async with self.db_pool.acquire() as conn:
+            next_task = await conn.fetchrow(
+                'SELECT id, date_trigger FROM scheduled_message ORDER BY date_trigger LIMIT 1;'
+            )
+
+        if next_task is None:
+            self._message_schedule_task.stop()
+
+        else:
+            await discord.utils.sleep_until(next_task['date_trigger'])
+
+            async with self.db_pool.acquire() as conn:
+                task = await conn.fetchrow(
+                    'SELECT * FROM scheduled_message WHERE id=$1;', next_task['id']
+                )
+
+            # task will be None if cancel command is triggered
+            if task is not None:
+                guild = self.bot.get_guild(task['guild_id'])
+                target_channel = guild.get_channel(task['channel_id'])
+
+                if target_channel is not None:
+                    await target_channel.send(content=task['message'])
+
+                async with self.db_pool.acquire() as conn:
+                    await conn.execute('DELETE FROM scheduled_message WHERE id = $1;', task['id'])
+
+    @_message_schedule_task.before_loop
+    async def _before_message_schedule_task(self):
+        await self.bot.wait_until_ready()
+
+    @staticmethod
+    def _parse_relative_time(time_text: str) -> Optional[Tuple]:
+        pattern = r"\d+[dhms]"
+        matched_list = re.findall(pattern, time_text)
+
+        if matched_list:
+            day = hour = minute = second = 0
+            for matched in matched_list:
+                number = int(matched[:-1])
+
+                if 'd' in matched:
+                    day += number
+                elif 'h' in matched:
+                    hour += number
+                elif 'm' in matched:
+                    minute += number
+                elif 's' in matched:
+                    second += number
+
+            return day, hour, minute, second
+
+        else:
+            return
 
 
 async def setup(bot: WarnetBot) -> None:
