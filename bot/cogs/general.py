6c876b1910d6fa7d0e83eb8cbf2dcf59bee84306
Iqrar Agalosi Nureyza <misteriqrar@gmail.com>
2023-05-02 11:26:08 +0700

    Add new activity

--- bot/cogs/general.py
+++ bot/cogs/general.py
@@ -222,6 +222,7 @@ class General(commands.Cog):
             discord.Game(name='PC WARNET'),
             discord.Game(name='Genshin Impact'),
             discord.Game(name='Arknights'),
+            discord.Game(name='Honkai: Star Rail'),
             discord.Activity(type=discord.ActivityType.listening, name=f'war! help'),
             discord.Activity(type=discord.ActivityType.watching, name=f'{humans} Pengguna WARNET'),
             discord.Activity(type=discord.ActivityType.competing, name='Spiral Abyss'),
