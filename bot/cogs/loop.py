2a4a867d5f881515630260c974157a164b21a3de
hilmoo <hlmmusyaffa@gmail.com>
2023-12-23 12:19:28 +0700

    fix some things

--- bot/cogs/loop.py
+++ bot/cogs/loop.py
@@ -1,12 +1,12 @@
 import io
-from datetime import datetime, time
+from datetime import datetime, time, timedelta, timezone
 
 import discord
 import pytz
 from discord.ext import commands, tasks
 
 from bot.bot import WarnetBot
-from bot.config import ADMIN_CHANNEL_ID, CustomRoleConfig, GUILD_ID
+from bot.config import Booster, GUILD_ID
 
 
 class Loop(commands.Cog):
@@ -17,18 +17,18 @@ class Loop(commands.Cog):
     async def on_connect(self) -> None:
         self._monthly_booster.start()
 
-    @tasks.loop(time=time(hour=18))
+    @tasks.loop(time=time(hour=0, minute=0, tzinfo=timezone(timedelta(hours=7))))
     async def _monthly_booster(self) -> None:
-        channel = self.bot.get_channel(ADMIN_CHANNEL_ID)
-        guild = self.bot.get_guild(GUILD_ID)
-        role = guild.get_role(CustomRoleConfig.BOOSTER_ROLE_ID)
-        members_content = ''
-        for member in role.members:
-            members_content += f"{member.id} "
-        buffer = io.BytesIO(members_content.encode('utf-8'))
-        file = discord.File(buffer, filename="honorary.txt")
         date = datetime.now(pytz.timezone('Asia/Jakarta'))
         if date.day == 1:
+            channel = self.bot.get_channel(Booster.ADMIN_CHANNEL_ID)
+            guild = self.bot.get_guild(GUILD_ID)
+            role = guild.get_role(Booster.BOOSTER_ROLE_ID)
+            members_content = ''
+            for member in role.members:
+                members_content += f"{member.id} "
+            buffer = io.BytesIO(members_content.encode('utf-8'))
+            file = discord.File(buffer, filename="honorary.txt")
             await channel.send(file=file)
 
     @_monthly_booster.before_loop
