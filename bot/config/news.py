586eeb5f88f47b44fbbb571b1010d5b09ce2df13
hilmoo <hilmusyaf2005@gmail.com>
2025-07-19 18:41:05 +0700

    remove unused config

--- bot/config/news.py
+++ bot/config/news.py
@@ -6,9 +6,6 @@ from bot import config
 
 INFORMATION_CHANNEL_ID = 774322083319775262 if config.BOT_DEBUG else 829136309271527434
 
-JSON_GENSHIN_NEWS_PATH = "bot/data/news/genshin.json"
-LAST_ID_GENSHIN_NEWS_PATH = "bot/data/news/genshin.txt"
-
 JSON_HOYOLAB_NEWS_PATH = "bot/data/news/hoyolab.json"
 LAST_ID_HOYOLAB_NEWS_PATH = "bot/data/news/hoyolab.txt"
 
