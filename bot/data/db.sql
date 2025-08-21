ac7f119dd0fdc48d357e217383a495594894ea99
Hilmi <68109788+hilmoo@users.noreply.github.com>
2025-08-21 12:06:09 +0700

    deprecated TCG cogs (#162)
    * deprecated TCG cogs
    
    * fix typo
    
    * remove some lint ignore

--- bot/data/db.sql
+++ bot/data/db.sql
@@ -1,18 +1,5 @@
 -- Script to initialize postgreSQL bot database
 
------- TCG FEATURE ------
--------------------------
-CREATE TABLE tcg_leaderboard(
-	discord_id BIGINT NOT NULL,
-	win_count INT DEFAULT 0 NOT NULL,
-	loss_count INT DEFAULT 0 NOT NULL,
-	elo FLOAT DEFAULT 1500 NOT NULL,
-	title BIGINT,
-	PRIMARY KEY(discord_id),
-	UNIQUE(discord_id)
-);
-CREATE INDEX IF NOT EXISTS tcg_leaderboard_discord_id_idx ON tcg_leaderboard (discord_id);
-
 ----- STICKY FEATURE -----
 --------------------------
 CREATE TABLE sticky(
